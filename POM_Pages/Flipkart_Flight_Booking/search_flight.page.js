4ba9c6d195f8c10c13f0426ac9c9d04afc70c69d
Chandrika <chandrikak173@gmail.com>
2025-10-07 10:52:14 +0530

    commit

--- POM_Pages/Flipkart_Flight_Booking/search_flight.page.js
+++ POM_Pages/Flipkart_Flight_Booking/search_flight.page.js
@@ -1,4 +1,13 @@
 export class SearchFlight
 {
-    
+    /**
+     * @param {import('playwright').Page} page
+     */
+
+    constructor(page)
+    {
+        this.page=page
+        this.searchBox=page.locator()
+
+    }
 }
