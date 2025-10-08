17e0ec6f1dd879e6c8bae8e87f616e918693e104
Chandrika <chandrikak173@gmail.com>
2025-10-08 10:49:11 +0530

    comm

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,6 @@
 // @ts-check
-//import { defineConfig, devices } from '@playwright/test';
-const { defineConfig, devices } = require('@playwright/test');
+import { defineConfig, devices } from '@playwright/test';
+//const { defineConfig, devices } = require('@playwright/test');
 
 /**
  * Read environment variables from file.
